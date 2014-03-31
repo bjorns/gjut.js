95b60d323f50feb3ec36cd0449af0d74ca3ddc94
Björn Skoglund <bjorn.skoglund@gmail.com>
2014-03-31 22:35:41 +0200

    Cleanup.

--- test/test_annotations.js
+++ test/test_annotations.js
@@ -1,32 +1,34 @@
 module.exports = {
 
-    setUp: function (callback) {
-        callback();
-    },
-    tearDown: function (callback) {
-        callback();
-    },
-    testFunctionCalled: function (test) {
-        annotations = require('../lib/annotations.js');
+  setUp: function (callback) {
+    callback();
+  },
+  tearDown: function (callback) {
+    callback();
+  },
+  testFunctionCalled: function (test) {
+    annotations = require('../lib/annotations.js');
+    imports = require('../lib/imports.js');
 
-        
-        var element = {
-                content: [],
-                annotation: {
-                    modulename: 'module',
-                    objectname: 'function'
-                }
-            },
-            context = {
-                'module': {
-                    'function': function(node) {
-                        element.callbackSuccessful = true;
-                    }
-                }
-            };
-
-        annotations.execute_annotations(element, context);
-        test.ok(element.callbackSuccessful);
-        test.done();
-    }
+    var element = {
+        content: [],
+        annotation: {
+          type: 'funcall',
+          module: 'module',
+          object: 'function'
+        }
+      },
+      context = imports.make_context();
+    
+    context['module'] = {
+      'function': function(node) {
+        element.callbackSuccessful = true;
+      }
+    };
+    
+    
+    annotations.execute_annotations(element, context);
+    test.ok(element.callbackSuccessful);
+    test.done();
+  }
 };
